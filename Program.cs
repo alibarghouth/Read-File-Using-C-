3b0299c9c666eef823704ddda27cca6c19fd4a07
alijbarghouth <alijbarghouth@gmail.com>
2022-09-19 17:00:40 +0300

    Version One

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Countries_Demo_App;
+using System.Security.Cryptography;
+
+namespace CountriesDemoApp{
+class Program
+{
+    static void Main(string[] args)
+    {
+           
+            String File = "C:\\Users\\user\\source\\repos\\Countries Demo App\\Country.Csv ";
+            CsvReader reader = new CsvReader(File);
+            List<Country> countries = reader.ReadFirstNCountry();
+
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"{country.Name} : {country.population}");
+            }
+    }
+} }
